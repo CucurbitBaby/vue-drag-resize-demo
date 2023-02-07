7e037c34d7c981154f3b8d8122c983f3da861eb2
DESKTOP-GPP6ANM\cyan <cucurbitbaby@163.com>
2023-02-07 22:05:56 +0800

    首次提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
+import * as svgicon from 'vue-svgicon'
 import App from './App.vue'
+import 'reset-css'
+
+
+Vue.use(ElementUI)
+
+Vue.use(svgicon, {
+  tagName: 'svgicon'
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
